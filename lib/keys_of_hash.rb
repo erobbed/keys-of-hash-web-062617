716d561700af61159ddc3ae1fefa903b9da1b2f0
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-13 20:53:53 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(arguments, *other_arguments)
+    array = []
+    self.each do |keys, values|
+      if arguments == values
+         array << keys
+      end
+      other_arguments.each do |args|
+        if args == values
+         array << keys
+        end
+      end
+    end
+    array
+    binding.pry
   end
 end
